960181b8cf804edbc37655127555ee2abd0a1168
CodeXrook <lacekitfly@gmail.com>
2020-10-06 14:43:57 -0400

    babel had a new update with @babel, after reintall app  now works

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './style.css';
 
-const WeatherApp = () => {
+const WeatherApp = () => (
     <div>
         <span><i className="fab fa-react"></i></span>
         <span>Weather App</span>
         <span><i className="wi wi-day-sunny"></i></span>
     </div>
-};
+);
 
 const ROOT_NODE = document.getElementById('root');
-ReactDom.render(<WeatherApp />, ROOT_NODE);
+ReactDOM.render(<WeatherApp />, ROOT_NODE);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const DIR_SRC = path.join(__dirname, 'src');
-const DIR_DIST =path.join(__dirname, 'dist');
+const DIR_DIST = path.join(__dirname, 'dist');
 
 const webpackConfig = {
     entry: path.join(DIR_SRC, 'index.js'),
@@ -26,10 +26,10 @@ const babelConfig = {
 }
 webpackConfig.module.rules.push(babelConfig);
 
-//CSS setup
+// CSS setup
 const cssConfig = {
-    test: /\.css4/,
-    exclude: /node_module/,
+    test: /\.css$/,
+    exclude: /node_modules/,
     use: ['style-loader', 'css-loader']
 }
 webpackConfig.module.rules.push(cssConfig);
